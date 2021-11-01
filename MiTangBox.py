76c540d5b941c0cc37539cd9d315a61049d54bae
Jacob Mee <jacobmee@gmail.com>
2021-11-01 23:11:08 +0800

    Run for Shairport-sync to get pipe data with theading

--- MiTangBox.py
+++ MiTangBox.py
@@ -11,10 +11,21 @@ import signal
 import sys
 import os
 import time
+from time import sleep
 import logging
 import subprocess
 import select
 import threading
+from item import Item
+import stat
+from util import write_data_to_image
+from codetable import CORE, SSNC, CORE_CODE_DICT, SSNC_CODE_DICT
+
+CORE_CODE_WHITELIST = {'mikd', 'minm', 'mper', 'miid', 'asal', 'asar', 'ascm', 'asco', 'asbr', 'ascp', 'asda', 'aspl',
+                       'asdm', 'asdc', 'asdn', 'aseq', 'asgn', 'asdt', 'asrv', 'assr', 'assz', 'asst', 'assp', 'astm',
+                       'astc', 'astn', 'asur', 'asyr', 'asfm', 'asdb', 'asdk', 'asbt', 'agrp', 'ascd', 'ascs', 'asct',
+                       'ascn', 'ascr', 'asri', 'asai', 'askd', 'assn', 'assu', 'aeNV', 'aePC', 'aeHV', 'aeMK', 'aeSN',
+                       'aeEN'}
 
 
 
@@ -32,85 +43,125 @@ class myThread(threading.Thread):
 
 
     def run(self):
-        self.log.debug("Watching starts")
         # Reading from file
-        f = subprocess.Popen(['cat','',"/home/pi/metadata/now_playing"],\
-            stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        p = select.poll()
-        p.register(f.stdout)
-
-        while True:
-            if p.poll(1):
-                l = f.stdout.readline()
-                # self.log.debug(l)
-                self.func(l.decode('utf8'))
-            time.sleep(0.5)
+        self._reading()
 
 
+    def _reading(self):
+        # wait till the pipe file is found
+        pipe_file =  "/tmp/shairport-sync-metadata"
+        while not os.path.exists(pipe_file) or not stat.S_ISFIFO(os.stat(pipe_file).st_mode):
+            self.log.warning("Could not find pipe: %s. Retrying in 5 seconds...", pipe_file)
+            sleep(5)
 
-class MiTangBox(QApplication):
-  def __init__(self, argv):
-    super().__init__(argv)
-    self.log = logging.getLogger("MiTangBox-display")
-    self.format = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s', "%Y-%m-%d %H:%M:%S")
-    self.handler = logging.StreamHandler(stream=sys.stdout)
-    self.handler.setFormatter(self.format)
-    self.handler.setLevel(logging.DEBUG)
-    self.log.addHandler(self.handler)
-    self.log.setLevel(logging.DEBUG)
-    self.log.info("Starting application")
+        self.log.info("Pipe watcher %s", pipe_file)
 
-    #self.length = 0
-    self.window = uic.loadUi("./MiTangBox.ui")
-    self.window.setStyleSheet("background-color : black; color : white;");
+        self._is_listening = True
+        self._tmp_track_info = {}   # temporary storage for track metadata
+        self.track_info = {}  # track info send by ssnc
+        self.artwork = ""
 
-    self.window.show()
-    self.mainArea = self.window.findChild(QLabel, 'main')
+        tmp = ""  # temporary string which stores one item
+        while self._is_listening:
+            with open(pipe_file) as pipe:
+                for line in pipe:
+                    # service was stopped
+                    if not self._is_listening:
+                        break
 
-    self._initialize_display()
-
-    # theading starts
-    thread1 = myThread(self._set_metadata)
-    thread1.start()
-    # theading ends
-    self.window.destroyed.connect(self.quit)
-
-
-  def _initialize_display(self):
-     self._set_metadata("artwork=default.jpg")
-
-
-  def _set_metadata(self, line):
-    line = line.rstrip("\n")
-    if line is None:
-        return
-
-    # What's the incoming message?
-    try:
-        key, value = line.split("=")
-    except Exception as e:
-        return
-
-    # self.log.debug("["+key+"]:" + value)
-
-    # Post to UI
-    if key == "artwork": # doing artwork
-        if value is not None:
-            artwork_path = "/home/pi/metadata/"+value
-            self.log.debug("Artwork:" + artwork_path)
-            pixmap = QPixmap(artwork_path)
-            if pixmap.width() >= pixmap.height():
-                self.mainArea.setPixmap(pixmap.scaledToWidth(200))
-            else:
-                self.mainArea.setPixmap(pixmap.scaledToHeight(200))
+                    strip_line = line.strip()
+                    if strip_line.endswith("</item>"):
+                        item = Item.item_from_xml_string(tmp + strip_line)
+                        if item:
+                            self._process_item(item)
+                        tmp = ""
+                    elif strip_line.startswith("<item>"):
+                        # if only a closing tag is missing we try to close the tag and try to parse the data
+                        if tmp != "":
+                            item = Item.item_from_xml_string(tmp + "</item>")
+                            if item:
+                                self._process_item(item)
+                        tmp = strip_line
+                    else:
+                        tmp += strip_line
 
 
-  def _clear_display(self):
-    self.mainArea.clear()
+    # pylint: disable=R0912, R0915
+    def _process_item(self, item):
+        """
+        Process a single item from the pipe.
+        :param item: metadata item
+        """
+
+        if item.type == SSNC:
+            # snua or snam are the 'ANNOUNCE' packet to reserve the player
+            if item.code == "PICT":
+                if item.data_base64:  # check if picture data is found
+                    self.artwork = write_data_to_image(item.data())  # Path to artwork image
+            elif item.code == "pcst":
+                # reset artwork
+                self.artwork = ""
+            elif item.code == "pcen":
+                # send artwork when all data is received
+                self.func(self.artwork)
+            elif item.code == "mden":
+                # only send updates if required
+                #if not (self._tmp_track_info.items() <= self.track_info.items()):
+                self.track_info = self._tmp_track_info
+                self._tmp_track_info = {}
+            # else:
+            #     self.log.debug("Unknown (ssnc) code \"%s\", with base64 data %s.", item.code,
+            #                    item.data_base64)
+
+        elif item.type == CORE:
+            if item.code in CORE_CODE_WHITELIST:
+                # save metadata info
+                dmap_key, data_type = CORE_CODE_DICT[item.code]
+                self._tmp_track_info[dmap_key] = item.data(dtype=data_type)
+            elif item.code in CORE_CODE_DICT:
+                # just ignore these and don't add them to the track info
+                # you can still listen to the item property to respond to these keys
+                pass
+            # else:
+            #     self.log.debug("Unknown core code: %s, with data %s.", item.code, item.data_base64)
+
+        self.item = item
+
+
+class mitangbox(QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
+        self.log = logging.getLogger("MiTangBox-display")
+        self.format = logging.Formatter('%(asctime)s - [%(levelname)s] - %(message)s', "%Y-%m-%d %H:%M:%S")
+        self.handler = logging.StreamHandler(stream=sys.stdout)
+        self.handler.setFormatter(self.format)
+        self.handler.setLevel(logging.DEBUG)
+        self.log.addHandler(self.handler)
+        self.log.setLevel(logging.DEBUG)
+
+        #self.length = 0
+        self.window = uic.loadUi("./mitangbox.ui")
+        self.window.setStyleSheet("background-color : black; color : white;");
+
+        self.window.show()
+        self.mainArea = self.window.findChild(QLabel, 'main')
+
+        self._set_metadata("./default.jpg")
+
+        # theading starts
+        thread1 = myThread(self._set_metadata)
+        thread1.start()
+        # theading ends
+
+
+    def _set_metadata(self,artwork_path):
+        self.log.info("Artwork:" + artwork_path)
+        pixmap = QPixmap(artwork_path)
+        self.mainArea.setPixmap(pixmap.scaledToWidth(240))
 
 
 if (__name__ == "__main__"):
-  client = MiTangBox(sys.argv)
-  signal.signal(signal.SIGINT, lambda *args: client.quit())
-  client.startTimer(500)
-  client.exec_()
+    client = mitangbox(sys.argv)
+    signal.signal(signal.SIGINT, lambda *args: client.quit())
+    client.startTimer(500)
+    client.exec_()
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb1
-python3 MiTangBox.py
+python3 mitangbox.py
