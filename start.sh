425eebbe19bb8e359f6c399e38a630eec36aff44
Jacob Mee <jacobmee@gmail.com>
2021-11-04 09:50:41 +0800

    V1.1

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
 export QT_QPA_PLATFORM=linuxfb:fb=/dev/fb1
 
-sudo ~/matrix/demo/nanopi-status/run.sh /dev/fb1
+cd /home/pi/matrix/demo/nanopi-status
+sudo run.sh /dev/fb1
+
+cd /home/pi/mitangbox
 python3 mitangbox.py
